848862a154d8cc3e5b809e6c7780381a0da41f91
Mikael Kall <kall.micke@gmail.com>
2019-01-28 15:35:09 +0100

    Added tclient.go draft

---
+++ tools/misc/tserver/tclient/tclient.go
@@ -0,0 +1,101 @@
+package main
+
+// File: tclient.go
+// Author: nighter@nighter.se
+// Not finalised yet.
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func printUsage() {
+	usage := `                                                                                                 
+	████████╗ ██████╗██╗     ██╗███████╗███╗   ██╗████████╗
+	╚══██╔══╝██╔════╝██║     ██║██╔════╝████╗  ██║╚══██╔══╝
+	   ██║   ██║     ██║     ██║█████╗  ██╔██╗ ██║   ██║   
+	   ██║   ██║     ██║     ██║██╔══╝  ██║╚██╗██║   ██║   
+	   ██║   ╚██████╗███████╗██║███████╗██║ ╚████║   ██║   
+	   ╚═╝    ╚═════╝╚══════╝╚═╝╚══════╝╚═╝  ╚═══╝   ╚═╝   		
+	Tclient [nighter@nighter.se] [Wrapsup plink.exe]
+	--------------------------------------------------------
+	-p <port to tunnel>	  |  The port you want to tunnel
+	-P <lport>                |  Override lport
+	-l <lhost>	          |  Override lhost
+	-w <password>             |  Override password
+	--------------------------------------------------------
+
+	Example: tclient -p 445
+
+Usage: tclient <arguments>
+`
+	fmt.Println(usage)
+	os.Exit(0)
+}
+
+func Shellout(command string) (error, string, string) {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command("bash", "-c", command)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	return err, stdout.String(), stderr.String()
+}
+
+func main() {
+
+	/* Settings */
+	var PORTUNNEL = "445"
+	var LHOST = "127.0.0.1"
+	var LPORT = "80"
+	var LPASSWORD = "password"
+
+	portTunnel := flag.String("p", "", "Port to tunnel")
+	lport := flag.String("P", "", "Ovveride lport")
+	lhost := flag.String("l", "", "Override lhost")
+	lpassword := flag.String("w", "", "Override password")
+
+	flag.Parse()
+
+	if *portTunnel == "" {
+		printUsage()
+	} else {
+		PORTUNNEL = *portTunnel
+	}
+
+	if *lport != "" {
+		LPORT = *lport
+	}
+
+	if *lhost != "" {
+		LHOST = *lhost
+	}
+
+	if *lpassword != "" {
+		LPASSWORD = *lpassword
+	}
+
+	// Should be replaced to the plink.exe command
+	command := []string{"echo", PORTUNNEL, " ", LPORT, " ", " ", LHOST, " ", LPASSWORD}
+
+	err, out, errout := Shellout(strings.Join(command, " "))
+	if err != nil {
+		log.Printf("error: %v\n", err)
+	}
+
+	fmt.Println(out)
+	fmt.Println(errout)
+
+}
